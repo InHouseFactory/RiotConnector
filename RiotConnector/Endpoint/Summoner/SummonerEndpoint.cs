703061ffaa7b27df82442ee154bc4a7559a1aa4a
Yann Plouvier <y.plouvier@cityscoot.eu>
2024-02-05 01:33:52 +0100

    fix: summoner ep

--- RiotConnector/Endpoint/Summoner/SummonerEndpoint.cs
+++ RiotConnector/Endpoint/Summoner/SummonerEndpoint.cs
@@ -26,7 +26,7 @@ public class SummonerEndpoint : ISummonerEndpoint
     public async Task<SummonerDto?> GetSummonerByNameAsync(GameRegion region, string summonerName)
     {
         var response =
-            await client.GetAsync<SummonerDto>(region, $"/riot/summoner/v1/summoners/by-name/{summonerName}");
+            await client.GetAsync<SummonerDto>(region, $"/lol/summoner/v4/summoners/by-name/{summonerName}");
         return response.StatusCode switch
         {
             HttpStatusCode.OK => JsonSerializer.Deserialize<SummonerDto>(response.Body),
@@ -37,7 +37,7 @@ public class SummonerEndpoint : ISummonerEndpoint
 
     public async Task<SummonerDto?> GetSummonerByPuuidAsync(GameRegion region, string puuid)
     {
-        var response = await client.GetAsync<SummonerDto>(region, $"/riot/summoner/v1/summoners/by-puuid/{puuid}");
+        var response = await client.GetAsync<SummonerDto>(region, $"/lol/summoner/v4/summoners/by-puuid/{puuid}");
         return response.StatusCode switch
         {
             HttpStatusCode.OK => JsonSerializer.Deserialize<SummonerDto>(response.Body),
@@ -49,7 +49,7 @@ public class SummonerEndpoint : ISummonerEndpoint
     public async Task<SummonerDto?> GetSummonerByAccountIdAsync(GameRegion region, string accountId)
     {
         var response =
-            await client.GetAsync<SummonerDto>(region, $"/riot/summoner/v1/summoners/by-account/{accountId}");
+            await client.GetAsync<SummonerDto>(region, $"/lol/summoner/v4/summoners/by-account/{accountId}");
         return response.StatusCode switch
         {
             HttpStatusCode.OK => JsonSerializer.Deserialize<SummonerDto>(response.Body),
@@ -60,7 +60,7 @@ public class SummonerEndpoint : ISummonerEndpoint
 
     public async Task<SummonerDto?> GetSummonerBySummonerIdAsync(GameRegion region, string summonerId)
     {
-        var response = await client.GetAsync<SummonerDto>(region, $"/riot/summoner/v1/summoners/{summonerId}");
+        var response = await client.GetAsync<SummonerDto>(region, $"/lol/summoner/v4/summoners/{summonerId}");
         return response.StatusCode switch
         {
             HttpStatusCode.OK => JsonSerializer.Deserialize<SummonerDto>(response.Body),
